3b7e30834bfa10941e9ed675a5630e893c066257
UPL-ANIME <nizomovsunnat0@gmail.com>
2026-03-06 23:00:57 -0800

    Add anime data structure in anim.js

---
+++ anim.js
@@ -0,0 +1,12 @@
+[
+  {
+    "id": "anime_1",
+    "title": "Naruto Uzumaki",
+    "thumb": "https://rasm-linki.jpg",
+    "turkum": ["Jangari", "Animatsiya"],
+    "qismlar": [
+      {"nom": "1-qism", "link": "YOUTUBE_ID_1"},
+      {"nom": "2-qism", "link": "YOUTUBE_ID_2"}
+    ]
+  }
+]
